d3ce907c2460ad7ea4a6f26de7ed4cd58ae3994d
Tho <thaivantho10b4@gmail.com>
2021-12-12 20:25:33 +0700

    dasda

--- FlashCard/app/src/main/java/team/loser/kanjiflashcard/fragments/AboutFragment.java
+++ FlashCard/app/src/main/java/team/loser/kanjiflashcard/fragments/AboutFragment.java
@@ -18,6 +18,7 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        //hahahahaha
         return inflater.inflate(R.layout.fragment_about, container, false);
     }
 }
